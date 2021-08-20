02e0f76fd6bc56dfda0e719867a2c8c2fa0ec6c9
ABHINAS ROY <85425565+roymusicplay@users.noreply.github.com>
2021-08-20 18:09:22 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -3,11 +3,11 @@ HEROKU = True   # NOTE Make it false if you're not deploying on heroku.
 # NOTE these values are for heroku.
 if HEROKU:
     from os import environ
-    API_ID = int(environ["API_ID"])
-    API_HASH = environ["API_HASH"]
-    SUDO_CHAT_ID = int(environ["SUDO_CHAT_ID"])
-    OWNER_ID = int(environ["OWNER_ID"])
-    SESSION_STRING = environ["SESSION_STRING"]
+    API_ID = int(environ["5969777"])
+    API_HASH = environ["f87a20613ac285f394cb2a949952698b"]
+    SUDO_CHAT_ID = int(environ["-1001161613115"])
+    OWNER_ID = int(environ["1957876808"])
+    SESSION_STRING = environ["BQASzNPGVPHtfBIdcJtJTK_SZMVssB8unaOmwGisgS-zc5gVnQfQJhDR2PgJ-X7qWkRitc_lJMVTu1qouGpJRX2prHA0jnmzm01B97l07zH-icQlOeVAJipDnJ120yiu2VR8bqJmXBUZiNjwIklXKmWUmRFjLa4UwFoMaUV0jYIPnYBESxfJ3LLDVXMYQ8JflcTYK48TF0wQjDQOPrw9qPEassHYTyw_pbdQcJvGbVV5ybX3oJFSyVs0vg9HyxElnH1CqT8AVFxJTsUUT6SkmpIitk4WKvTIeLw_vQ66xw9VJF3TgD_IBGMGj7dXU0ifRwH0dWQa8U4NUxU0BISrcvdLLUSAA"]
 
 # NOTE Fill this if you are not deploying on heroku.
 if not HEROKU:
